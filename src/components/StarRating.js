d508097843f729783b4a0b08df3e221d18fa9a85
Kamau-sam <skamau.6411@gmail.com>
2024-05-06 16:14:44 +0300

    Added comments to the component

--- src/components/StarRating.js
+++ src/components/StarRating.js
@@ -1,17 +1,18 @@
 import React, { useState } from "react";
 
 function StarRating({ onRating }) {
-  const [rating, setRating] = useState(0);
+  const [rating, setRating] = useState(0); // State to keep track of the current rating
 
   function handleClick(index) {
     setRating(index);
     onRating(index);
   }
-
+  // Render a list of buttons for the star ratings
   return (
     <div>
       {[...Array(5)].map((star, index) => {
-        index += 1;
+        //Map over an array of 5 elements to create a star rating interface
+        index += 1; // Adjust index to start on 1
         return (
           <button
             type="button"
@@ -23,5 +24,5 @@ function StarRating({ onRating }) {
     </div>
   );
 }
-
+// Export the StarRating component as the default export
 export default StarRating;
