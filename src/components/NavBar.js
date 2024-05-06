97cd413e7d8afb6ee2ead25d5bfa8d80a88c010e
briimiin <kipkorirbrimin005@gmail.com>
2024-05-06 23:52:49 +0300

    Codes for NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./NavBar.css"
+
+function NavBar () {
+    return (
+        <div className="navbar">
+
+            <ul>
+                <li>Profile</li>
+                <li>LoveForm</li>
+                <li>FoodForDate</li>
+            </ul>
+        </div>
+    )
+}
