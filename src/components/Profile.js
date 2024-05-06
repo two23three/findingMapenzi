9da4538a0c09ea0ffed238c876c7a5ec6c368e52
Teddy <teddykipchumba17@gmail.com>
2024-05-06 20:24:22 +0300

    Ted

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -81,8 +81,8 @@ function Profile() {
               onChange={handleGenderChange}
             >
               <option value="">Select</option>
-              <option value="male">Male</option>
-              <option value="female">Female</option>
+              <option value="Male">Male</option>
+              <option value="Female">Female</option>
             </select>
           </label>
           <label>
@@ -93,7 +93,25 @@ function Profile() {
               onChange={handleInputChange}
             />
           </label>
-          <button type="submit">Save Changes</button>
+          <button className='submit' type='submit'>
+  <div class="svg-wrapper-1">
+    <div class="svg-wrapper">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        width="24"
+        height="24"
+      >
+        <path fill="none" d="M0 0h24v24H0z"></path>
+        <path
+          fill="currentColor"
+          d="M1.946 9.315c-.522-.174-.527-.455.01-.634l19.087-6.362c.529-.176.832.12.684.638l-5.454 19.086c-.15.529-.455.547-.679.045L12 14l6-8-8 6-8.054-2.685z"
+        ></path>
+      </svg>
+    </div>
+  </div>
+  <span>Submit</span>
+</button>
         </form>
       ) : (
         <div className="profile-details">
