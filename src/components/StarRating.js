db33aece188e9a17b56907a3239ba68c16d5a03a
Kamau-sam <skamau.6411@gmail.com>
2024-05-06 15:55:01 +0300

    Implemented clikable stars

--- src/components/StarRating.js
+++ src/components/StarRating.js
@@ -1,5 +1,27 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
 function StarRating({ onRating }) {
   const [rating, setRating] = useState(0);
+
+  function handleClick(index) {
+    setRating(index);
+    onRating(index);
+  }
+
+  return (
+    <div>
+      {[...Array(5)].map((star, index) => {
+        index += 1;
+        return (
+          <button
+            type="button"
+            key={index}
+            onClick={() => handleClick(index)}
+          ></button>
+        );
+      })}
+    </div>
+  );
 }
+
+export default StarRating;
