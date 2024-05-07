1d4b4c3ec088f2350b44dbfe86b2112ab96c66ea
Kamau-sam <skamau.6411@gmail.com>
2024-05-07 11:49:58 +0300

    uhooked Rating Component from the App

--- src/App.js
+++ src/App.js
@@ -3,15 +3,12 @@ import Profile from "./components/Profile";
 import React from "react";
 import LoveSurvey from "./components/LoveSurvey";
 import FoodDisplay from "./components/FoodDisplay";
-import StarRating from "./components/StarRating";
-
 function App() {
   return (
     <div className="App">
       <Profile />
       <LoveSurvey />
       <FoodDisplay />
-      <StarRating />
     </div>
   );
 }
--- src/components/FoodDisplay.js
+++ src/components/FoodDisplay.js
@@ -1,29 +1,31 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 function FoodDisplay() {
   const [recipes, setRecipes] = useState([]);
-  const API_KEY = ''; // 3c78c7ebf8cf4dbf88d442a2a8591e8a has 150 daily quota
+  const API_KEY = ""; // 3c78c7ebf8cf4dbf88d442a2a8591e8a has 150 daily quota
 
-  useEffect(function() {
-    fetch(`https://api.spoonacular.com/recipes/random?number=5&apiKey=${API_KEY}`)
-      .then(function(response) {
+  useEffect(function () {
+    fetch(
+      `https://api.spoonacular.com/recipes/random?number=5&apiKey=${API_KEY}`
+    )
+      .then(function (response) {
         if (!response.ok) {
-          throw new Error('Network response was not ok');
+          throw new Error("Network response was not ok");
         }
         return response.json();
       })
-      .then(function(data) {
+      .then(function (data) {
         setRecipes(data.recipes);
       })
-      .catch(function(error) {
-        console.error('Error fetching recipes:', error);
+      .catch(function (error) {
+        console.error("Error fetching recipes:", error);
       });
   }, []);
 
   return (
     <div>
       <h1>Random Foods</h1>
-      {recipes.map(function(recipe, index) {
+      {recipes.map(function (recipe, index) {
         return (
           <div key={index}>
             <h2>{recipe.title}</h2>
@@ -31,7 +33,7 @@ function FoodDisplay() {
             <p>Ready in {recipe.readyInMinutes} minutes</p>
             <p>Servings: {recipe.servings}</p>
             <ul>
-              {recipe.extendedIngredients.map(function(ingredient, i) {
+              {recipe.extendedIngredients.map(function (ingredient, i) {
                 return <li key={i}>{ingredient.original}</li>;
               })}
             </ul>
