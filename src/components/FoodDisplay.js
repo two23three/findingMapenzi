0325e65e3f20e0f260491b3d6070a82ae8e76bbb
Migero <mikeroche138@gmail.com>
2024-05-07 11:59:45 +0300

    add ClassName

--- src/components/FoodDisplay.js
+++ src/components/FoodDisplay.js
@@ -3,7 +3,7 @@ import './FoodDisplay.css'; // Import CSS file for styling
 
 function FoodDisplay() {
   const [recipes, setRecipes] = useState([]);
-  const API_KEY = ''; // 3c78c7ebf8cf4dbf88d442a2a8591e8a has 150 daily quota
+  const API_KEY = '3c78c7ebf8cf4dbf88d442a2a8591e8a'; // 3c78c7ebf8cf4dbf88d442a2a8591e8a has 150 daily quota
 
   useEffect(() => {
     fetch(`https://api.spoonacular.com/recipes/random?number=5&apiKey=${API_KEY}`)
@@ -24,20 +24,22 @@ function FoodDisplay() {
   return (
     <div className="food-container">
       <h1 className="food-heading">Random Foods with recipes</h1>
-      {recipes.map((recipe, index) => (
-        <div key={index} className="recipe-card">
-          <h2 className="recipe-title">{recipe.title}</h2>
-          <img src={recipe.image} alt={recipe.title} className="recipe-image" />
-          <p className="recipe-info">Ready in {recipe.readyInMinutes} minutes</p>
-          <p className="recipe-info">Servings: {recipe.servings}</p>
-          <ul className="recipe-ingredients">
-            {recipe.extendedIngredients.map((ingredient, i) => (
-              <li key={i}>{ingredient.original}</li>
-            ))}
-          </ul>
-          <a href={recipe.sourceUrl} className="recipe-link">View Recipe</a>
-        </div>
-      ))}
+      <div className="recipe-grid">
+        {recipes.map((recipe, index) => (
+          <div key={index} className="recipe-card">
+            <h2 className="recipe-title">{recipe.title}</h2>
+            <img src={recipe.image} alt={recipe.title} className="recipe-image" />
+            <p className="recipe-info">Ready in {recipe.readyInMinutes} minutes</p>
+            <p className="recipe-info">Servings: {recipe.servings}</p>
+            <ul className="recipe-ingredients">
+              {recipe.extendedIngredients.map((ingredient, i) => (
+                <li key={i}>{ingredient.original}</li>
+              ))}
+            </ul>
+            <a href={recipe.sourceUrl} className="recipe-link">View Recipe</a>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
