e1e214c9bf7102ca2b5965af27a36c515baf1876
Tulley Mwenda <Tulleyteyez@gmail.com>
2024-05-07 14:40:31 +0300

    remove 2nd navbar

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import Profile from "./components/Profile";
 import React from "react";
 import FoodDisplay from "./components/FoodDisplay";
 import StarRating from "./components/StarRating";
-//imports navbar
-import NavBar from "./components/NavBar"; // Import NavBar
+
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
@@ -12,8 +11,7 @@ function App() {
   return (
     <Router>
     <div className="App">
-      {/*routes for navbar*/}
-    <NavBar />
+     
       <Routes>
         <Route path="/" element={<Profile />} />
         <Route path="/food-display" element={<FoodDisplay />} />
