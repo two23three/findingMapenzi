54aaabbbe4b3507c647fda1398c35cd5e0733377
Tulley Mwenda <Tulleyteyez@gmail.com>
2024-05-08 23:05:15 +0300

    added a loading screen

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState , useEffect} from "react";
 import "./App.css";
 import Profile from "./components/Profile";
 import FoodDisplay from "./components/FoodDisplay";
@@ -6,26 +6,38 @@ import LoveSurvey from "./components/LoveSurvey";
 import RatedFoods from "./components/RatedFoods";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "./ThemeContext"; // Import ThemeProvider from ThemeContext file
-
+import LoadScreen from "./components/LoadScreen";
 function App() {
   const [ratings, setRatings] = useState({}); // Initialize state for ratings
+  const [isLoading, setIsLoading] = useState(true);
 
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setIsLoading(false);
+    }, 2000); // Adjust the delay time as needed
+
+    return () => clearTimeout(timeout);
+  }, []);
   return (
     <ThemeProvider>
       <Router>
         <div className="App">
-          <Routes>
-            <Route path="/" element={<Profile />} />
-            <Route
-              path="/food-display"
-              element={<FoodDisplay setRatings={setRatings} />}
-            />
-            <Route path="/lovesurvey" element={<LoveSurvey />} />
-            <Route
-              path="/rated-foods"
-              element={<RatedFoods ratings={ratings} />}
-            />
-          </Routes>
+          {isLoading ? (
+            <LoadScreen />
+          ) : (
+            <Routes>
+              <Route path="/" element={<Profile />} />
+              <Route
+                path="/food-display"
+                element={<FoodDisplay setRatings={setRatings} />}
+              />
+              <Route path="/lovesurvey" element={<LoveSurvey />} />
+              <Route
+                path="/rated-foods"
+                element={<RatedFoods ratings={ratings} />}
+              />
+            </Routes>
+          )}
         </div>
       </Router>
     </ThemeProvider>
