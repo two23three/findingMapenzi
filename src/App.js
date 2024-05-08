105c21fd255069b5556739dcf22b6eeac9091dc9
briimiin <kipkorirbrimin005@gmail.com>
2024-05-08 11:16:48 +0300

    removed navbar from app

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ function App() {
   return (
     <Router>
     <div className="App">
-     <NavBar />
       <Routes>
         <Route path="/" element={<Profile />} />
         <Route path="/food-display" element={<FoodDisplay />} />
