6ca42a1db7abca3ad6ddef7111c73870af8e2c87
Tulley Mwenda <Tulleyteyez@gmail.com>
2024-05-08 13:41:03 +0300

    remove key

--- src/components/FoodDisplay.js
+++ src/components/FoodDisplay.js
@@ -8,7 +8,7 @@ function FoodDisplay() {
   const [ratings, setRatings] = useState({});
   const [searchQuery, setSearchQuery] = useState("");
 
-  const API_KEY = "3c78c7ebf8cf4dbf88d442a2a8591e8a";
+  const API_KEY = "";//3c78c7ebf8cf4dbf88d442a2a8591e8a
 
   useEffect(() => {
     if (searchQuery.trim() !== "") {
