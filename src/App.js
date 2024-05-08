6898eb2ae01b9f2f459a6790d3198e856fca9a8c
briimiin <kipkorirbrimin005@gmail.com>
2024-05-08 13:56:59 +0300

    id for light

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
 
   return (
     <Router>
-      <div className="App">
+      <div className="App" id="light">
         <Routes>
           <Route path="/" element={<Profile />} />
           <Route
