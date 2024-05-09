ed2cd3a2a8554ccf720c309783f060245ad40322
Kamau-sam <skamau.6411@gmail.com>
2024-05-09 09:00:44 +0300

    Edited and restyled the stars

--- src/components/StarRating.js
+++ src/components/StarRating.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import "./StarRating.css";
 
 function StarRating({ onRating }) {
-  // onRating is a callback function passed as a prop
   const [rating, setRating] = useState(0);
 
   function handleClick(index) {
@@ -12,17 +11,16 @@ function StarRating({ onRating }) {
 
   return (
     <div className="star-rating-container">
-      {[...Array(5)].map((star, index) => {
-        // Map over an array of 5 elements to create the stars
-        index += 1; // Adjust the index to start from 1 instead of 0
+      {[...Array(5)].map((_, index) => {
+        index += 1;
         return (
           <button
             type="button"
             key={index}
-            className={index <= rating ? "star-button selected" : "star-button"} // Apply 'selected' class if the star is part of the current rating
+            className="star-button"
             onClick={() => handleClick(index)}
           >
-            <span className="star">&#9733;</span> {/* Star icon */}
+            <span className={index <= rating ? "star selected" : "star"}>&#9733;</span>
           </button>
         );
       })}
